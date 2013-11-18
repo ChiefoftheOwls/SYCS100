77d2c7990342af3effcfc3440fbaea677b778a4a
Elyon Olaniran <ElyonOlaniran@outlook.com>
2013-11-17 21:58:18 -0500

    Update menjossearch.py

--- projects/bsearch/menjossearch.py
+++ projects/bsearch/menjossearch.py
@@ -161,3 +161,31 @@ def bsearch(blist,searchElement):
                 return -1
 
 #Oreoluwa Onatemowo's code ends here
+#Elyon Olaniran
+List = [1, 2, 3, 5, 7, 8, 9, 12, 14, 15, 59, 61, 78, 84]
+'''binary search only works on a sorted list. for the sake of this code.
+I created a sorted List, hence no need to sort it again.'''
+Element = 84
+def bsearch(List, Element):
+        if List == []:
+                print 'empty list'
+        else:
+                x = len(List)
+                q = 0
+                first = 0
+                last = x-1
+                midpoint = (first + last)//2
+                for q in range(x):
+                        if Element == List[midpoint]:
+                                return midpoint
+                        elif Element < List[midpoint]:
+                                last = midpoint-1
+                                midpoint = (first+last)//2
+                        elif Element > List[midpoint]:
+                                first = midpoint + 1
+                                midpoint = (first+last)//2
+                        else:
+                                return -1
+print bsearch(List, Element)
+
+#Elyon Olaniran code ends
